58b7a2cf5ec75cef1e60d1a81d611f6a887f664c
Don Roberts <roberts@refactory.com>
2013-10-03 11:15:22 -0500

    Initialize rails application for purveyor.

---
+++ purveyor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purveyor::Application.config.secret_token = '67d561a4a9c59ee6eeabf465a970531d8c95dd64e7d1245b46cf807f2f6f0581871c836154db5bb5f137a8c5faf43eaf2d2dd5830267c22ce7eea9dbd8d37aae'
