97f952f03ae8d066aa441d3affc84426bad86236
Jeff <jeff@jeff-VirtualBox.(none)>
2013-11-11 16:13:46 -0600

    added new methods to controller

--- purveyor/app/controllers/customerform_controller.rb
+++ purveyor/app/controllers/customerform_controller.rb
@@ -4,13 +4,25 @@ class CustomerformController < ApplicationController
     render :partial => "customerform/customerform"
   end
 
+  def list
+    @customer = Customer.find(:all)
+  end
+
   def new
+    @customer = Customer.new
   end
 
   def create
-    render text: params[:customerform].inspect
+    #render text: params[:customerform].inspect
+    @customer = Customer.new(params[:customer])
+    if @customer.save
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 end
