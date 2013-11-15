3d967ba299f179858d80c87c56cbaa7b147a10bc
Alysas Matthews <am458@evansville.edu>
2013-11-15 10:14:10 -0600

    created a Session model  -am458, nm115

--- purveyor/db/schema.rb
+++ purveyor/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131114193122) do
+ActiveRecord::Schema.define(version: 20131115160902) do
 
   create_table "admins", force: true do |t|
     t.string   "username"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20131114193122) do
   add_index "legacy_session_table", ["updated_at"], name: "index_legacy_session_table_on_updated_at"
 
   create_table "new_users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sessions", force: true do |t|
     t.string   "username"
     t.string   "password"
     t.datetime "created_at"
