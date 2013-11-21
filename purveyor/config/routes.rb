03312ae3566d4870e3c96534eb9fe9d8f11bc4cb
Chelsea <ch250@ubuntu.(none)>
2013-11-20 18:29:57 -0600

    Added index

--- purveyor/config/routes.rb
+++ purveyor/config/routes.rb
@@ -2,7 +2,7 @@ Purveyor::Application.routes.draw do
   resources :farmers_form
   resources :customerform
   resources :admins
-  resources :inventory
+  resources :inventories
   root to: "main_menu#index"
   
 end
