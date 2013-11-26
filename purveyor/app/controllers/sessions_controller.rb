8e6454bc6d74a748bbdf56dbbd5fdb1cfe0fc829
Nathan <nathan@nathan-VirtualBox.(none)>
2013-11-26 08:21:59 -0600

    Main Menu / Sign Out links no longer appear at sign-in. Bad login now redirects rather than rendering 'new.' nm115

--- purveyor/app/controllers/sessions_controller.rb
+++ purveyor/app/controllers/sessions_controller.rb
@@ -3,19 +3,18 @@ class SessionsController < ApplicationController
   skip_before_filter :authorize
 
   def new
+    render :layout => false
   end
 
   def create
     @admin = Admin.find_by(username: params[:session][:username])
     if @admin && @admin.authenticate(params[:session][:password])
       sign_in @admin
-    #  if signed_in?
-      redirect_to session[:original_url] 
     else
       flash.now[:error] = "Invalid Username/Password Combination"
       flash.keep
-      render 'new'
     end
+    redirect_to session[:original_url]
   end
  
   def destroy
