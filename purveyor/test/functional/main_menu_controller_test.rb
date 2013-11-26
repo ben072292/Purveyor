7a3d1911ac4f2baa4801ddd2bed297583ea6fa18
Don Roberts <roberts@refactory.com>
2013-11-26 11:57:21 -0600

    fixed some of the redirect failures in the tests by signing in before running.

--- purveyor/test/functional/main_menu_controller_test.rb
+++ purveyor/test/functional/main_menu_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class MainMenuControllerTest < ActionController::TestCase
+  setup do
+    sign_in(admins(:one))
+  end
+
   # Replace this with your real tests.
   test "make sure root route works" do
-    get :index
+    get :index, nil
     assert_response :success
     assert_select 'title', "Main Menu"
   end
