574cf99e9af2fcbb96c19ce61148ea6ab52c3574
Weicong Chen <wc26@evansville.edu>
2013-12-10 09:33:30 -0600

    Add destroy method for farmer/customer inventories

--- purveyor/app/controllers/customer_inventories_controller.rb
+++ purveyor/app/controllers/customer_inventories_controller.rb
@@ -4,4 +4,11 @@ class CustomerInventoriesController < ApplicationController
       @customer_inventory = @customer.custormer_inventories.create(params[:customer_inventory].permit(:name))
       redirect_to customer_path(@customer)
     end
+    
+  def destroy
+    @customer = Customer.find(params[:name_id])
+    @customer_inventory = CustomerInventory.find(params[:id])
+    @customer_inventory.destroy
+    redirect_to customer_path(@customer)
+  end
 end
--- purveyor/app/controllers/farmer_inventories_controller.rb
+++ purveyor/app/controllers/farmer_inventories_controller.rb
@@ -4,4 +4,11 @@ class FarmerInventoriesController < ApplicationController
     @farmer_inventory = @farmer.farmer_inventories.create(params[:farmer_inventory].permit(:name))
     redirect_to farmer_path(@farmer)
   end
+  
+  def destroy
+    @farmer = Farmer.find(params[:name_id])
+    @farmer_inventory = FarmerInventory.find(params[:id])
+    @farmer_inventory.destroy
+    redirect_to farmer_path(@farmer)
+  end
 end
